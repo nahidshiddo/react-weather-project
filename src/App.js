9d371a38db9deb2cc46b990315f15bbad0510545
Nahid Shiddo <nahid.shiddo@gmail.com>
2022-06-18 18:31:20 -0400

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App </h1>
+<footer>
+  This project was coded by Nahid S and is
+    <a href="https://github.com/nahidshiddo/react-weather-project" target="_blank" rel="noreferrer"> open sourced on github</a>
+</footer>
+
     </div>
   );
 }
