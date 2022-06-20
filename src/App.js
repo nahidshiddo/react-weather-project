3cc6be2cbd60821e955c64d69baab1855b786ad7
Nahid Shiddo <nahid.shiddo@gmail.com>
2022-06-19 22:36:50 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App </h1>
 <footer>
   This project was coded by Nahid S and is
     <a href="https://github.com/nahidshiddo/react-weather-project" target="_blank" rel="noreferrer"> open sourced on github</a>
 </footer>
-
+</div>
     </div>
   );
 }
