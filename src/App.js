341fd22fdcb88da62c6f6a3e7db8fbb40853f010
Nahid Shiddo <nahid.shiddo@gmail.com>
2022-06-20 02:43:40 -0400

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <Weather />
 <footer>
   This project was coded by Nahid S and is
-    <a href="https://github.com/nahidshiddo/react-weather-project" target="_blank" rel="noreferrer"> open sourced on github</a>
+    <a href="https://github.com/nahidshiddo/react-weather-project" target="_blank" rel="noreferrer" className="footer"> open sourced on github</a>
 </footer>
 </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -4,45 +4,38 @@ import "./Weather.css"
 export default function Weather(){
     return <div className="Weather">
       <form>
-        
         <div className="row">
             <div className="col-9">
-        <input type="search" placeholder="Enter a city..." className="form-control" />
+        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
         </div>
         <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary"/>
+        <input type="submit" value="Search" className="btn btn-primary w-100"/>
         </div>
         </div>
       </form>
-
-     
         <h1>New York</h1>
         <ul>
             <li>
                 Wed 07:00
             </li>
             <li>Cloudy</li>
-        
         </ul>
-
+        
         <div className="row">
-<div className="col-6">
-    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-    6°C
-</div>
-<div className="col-6">
-    <ul>
-        <li>Precipitaiton: 15%</li>
-        <li>Humidity: 60%</li>
-        <li>Wind: 10 km/h</li>
-       
-    </ul>
-</div>
+            <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" /> 
 
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+            </div>
 
+        <div className="col-6">
+        <ul>
+            <li>Precipitaiton: 15%</li>
+            <li>Humidity: 60%</li>
+            <li>Wind: 10 km/h</li>
+        </ul>
         </div>
-        
-        
-        
+        </div>
         </div>
 }
