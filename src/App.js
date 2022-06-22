a9e035f1e8102f5c70a559f16923cf7926f5e8a3
Nahid Shiddo <nahid.shiddo@gmail.com>
2022-06-22 01:16:06 -0400

    Added fahrenheit conversion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
+
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Berlin" />
+        <Weather defaultCity="Toronto" />
+
         <footer>
           This project was coded by Nahid S and is
           <a
