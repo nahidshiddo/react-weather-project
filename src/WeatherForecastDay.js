6cd085ee39d679c2d816b010393b9fc2b5674a50
Nahid Shiddo <nahid.shiddo@gmail.com>
2022-07-03 21:42:30 -0400

    Made correction to forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -14,7 +14,7 @@ export default function WeatherForecastDay(props) {
 
   function day() {
     let date = new Date(props.data.dt * 1000);
-    let day = date.getDate();
+    let day = date.getDay();
 
     let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
 
